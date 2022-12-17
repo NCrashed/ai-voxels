aff82e82b3fe629098259fd19cafd62cb18587e2
Anton Gushcha <ncrashed@protonmail.com>
2022-12-17 21:27:35 +0700

    Better try

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,172 @@
-extern crate sdl2;
+use sdl2::{
+    event::Event,
+    keyboard::Keycode,
+    mouse::MouseButton,
+    render::{Canvas, TextureCreator},
+    video::{Window, WindowContext},
+};
 
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use sdl2::rect::Rect;
+use cgmath::{EuclideanSpace, SquareMatrix};
+use rand::prelude::*;
 
-use std::f64::consts::PI;
-use std::thread;
-use std::time::Duration;
+use cgmath::{Deg, Matrix4, PerspectiveFov, Point3, Rad, Vector3};
 
 const WINDOW_WIDTH: u32 = 640;
 const WINDOW_HEIGHT: u32 = 480;
-const VOXEL_SIZE: u32 = 16;
-const SPHERE_RADIUS: i32 = 16;
-const LIGHT_ANGLE: f64 = PI / 4.0;
+
+const FOV: f32 = 60.0;
+const NEAR_PLANE: f32 = 0.1;
+const FAR_PLANE: f32 = 100.0;
 
 struct Camera {
-    x: f64,
-    y: f64,
-    z: f64,
-    angle: f64,
+    position: Point3<f32>,
+    rotation: Vector3<f32>,
 }
 
 impl Camera {
-    fn new() -> Camera {
-        Camera {
-            x: 8.0,
-            y: 8.0,
-            z: SPHERE_RADIUS as f64,
-            angle: 0.0,
+    fn projection_matrix(&self) -> Matrix4<f32> {
+        let aspect_ratio = WINDOW_WIDTH as f32 / WINDOW_HEIGHT as f32;
+        PerspectiveFov {
+            fovy: Deg(FOV).into(),
+            aspect: aspect_ratio,
+            near: NEAR_PLANE,
+            far: FAR_PLANE,
         }
+        .to_perspective()
+        .into()
     }
 
-    fn rotate(&mut self, angle: f64) {
-        self.angle += angle;
+    fn view_matrix(&self) -> Matrix4<f32> {
+        Matrix4::look_at(
+            self.position,
+            Point3::new(0.0, 0.0, 0.0),
+            Vector3::new(0.0, 1.0, 0.0),
+        ) * Matrix4::from_angle_x(Rad(self.rotation.x))
+            * Matrix4::from_angle_y(Rad(self.rotation.y))
+            * Matrix4::from_angle_z(Rad(self.rotation.z))
     }
 }
 
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+struct Voxel {
+    position: Point3<f32>,
+    color: (u8, u8, u8),
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
     let window = video_subsystem
-        .window("Voxel Sphere", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .window("Voxel Grid", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .build()
-        .unwrap();
-    let mut renderer = window.into_canvas().build().unwrap();
+        .map_err(|e| e.to_string())?;
+    let mut canvas = window
+        .into_canvas()
+        .accelerated()
+        .build()
+        .map_err(|e| e.to_string())?;
 
-    let mut camera = Camera::new();
+    let texture_creator: TextureCreator<_> = canvas.texture_creator();
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    'running: loop {
+    let mut voxels: Vec<Voxel> = Vec::new();
+
+    let radius = 5;
+    for x in -radius..=radius {
+        for y in -radius..=radius {
+            for z in -radius..=radius {
+                let position = Point3::new(x as f32, y as f32, z as f32);
+                let distance =
+                    (position.x.powi(2) + position.y.powi(2) + position.z.powi(2)).sqrt();
+                if distance < radius as f32 {
+                    let color = (
+                        (position.x * 255.0 / radius as f32) as u8,
+                        (position.y * 255.0 / radius as f32) as u8,
+                        (position.z * 255.0 / radius as f32) as u8,
+                    );
+                    voxels.push(Voxel { position, color });
+                }
+            }
+        }
+    }
+
+    let mut camera = Camera {
+        position: Point3::new(0.0, 0.0, 10.0),
+        rotation: Vector3::new(0.0, 0.0, 0.0),
+    };
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    let mut running = true;
+    while running {
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                } => break 'running,
+                } => {
+                    running = false;
+                }
+                Event::MouseMotion {
+                    xrel,
+                    yrel,
+                    mousestate,
+                    ..
+                } => {
+                    if mousestate.left() {
+                        camera.rotation.x += yrel as f32 / 100.0;
+                        camera.rotation.y -= xrel as f32 / 100.0;
+                    }
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::W),
+                    ..
+                } => {
+                    camera.position.z -= 0.1;
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::S),
+                    ..
+                } => {
+                    camera.position.z += 0.1;
+                }
                 _ => {}
             }
         }
 
-        renderer.set_draw_color(Color::RGB(0, 0, 0));
-        renderer.clear();
+        canvas.set_draw_color((0, 0, 0, 255));
+        canvas.clear();
 
-        // render voxels
-        for x in -SPHERE_RADIUS..=SPHERE_RADIUS {
-            for y in -SPHERE_RADIUS..=SPHERE_RADIUS {
-                for z in -SPHERE_RADIUS..=SPHERE_RADIUS {
-                    let distance = ((x * x) + (y * y) + (z * z)) as f64;
-                    if distance <= (SPHERE_RADIUS as f64) * (SPHERE_RADIUS as f64) {
-                        let projection = project(x as f64, y as f64, z as f64, &camera);
-                        let (screen_x, screen_y) = (
-                            projection.x as i32 + (WINDOW_WIDTH as i32 / 2),
-                            projection.y as i32 + (WINDOW_HEIGHT as i32 / 2),
-                        );
-                        let voxel_rect = Rect::new(screen_x, screen_y, VOXEL_SIZE, VOXEL_SIZE);
-                        let voxel_color = calculate_lighting(x as f64, y as f64, z as f64, &camera);
-                        renderer.set_draw_color(voxel_color);
-                        renderer.fill_rect(voxel_rect).unwrap();
-                    }
-                }
-            }
+        let projection = camera.projection_matrix();
+        let view = camera.view_matrix();
+
+        for voxel in &voxels {
+            let clip_vec = (projection * view) * voxel.position.to_homogeneous();
+
+            let (x, y) = (
+                ((clip_vec.x / clip_vec.w + 1.0) * WINDOW_WIDTH as f32 / 2.0) as i32,
+                ((clip_vec.y / clip_vec.w + 1.0) * WINDOW_HEIGHT as f32 / 2.0) as i32,
+            );
+
+            let voxel_size = 16;
+            let mut texture = texture_creator
+                .create_texture_static(sdl2::pixels::PixelFormatEnum::RGB24, voxel_size, voxel_size)
+                .map_err(|e| e.to_string())?;
+
+            let pixel_data: Vec<u8> = [voxel.color.0, voxel.color.1, voxel.color.2]
+                .into_iter()
+                .cycle()
+                .take((voxel_size * voxel_size) as usize)
+                .collect();
+            texture
+                .update(None, &pixel_data, 3)
+                .map_err(|e| e.to_string())?;
+
+            let target = sdl2::rect::Rect::new(x, y, voxel_size, voxel_size);
+            canvas.copy(&texture, None, Some(target))?;
         }
-        renderer.present();
-
-        camera.rotate(0.01);
-
-        thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        canvas.present();
     }
-}
 
-fn project(x: f64, y: f64, z: f64, camera: &Camera) -> Point {
-    let mut x = x - camera.x;
-    let mut y = y - camera.y;
-    let sin_angle = camera.angle.sin();
-    let cos_angle = camera.angle.cos();
-
-    let new_x = x * cos_angle - y * sin_angle;
-    y = x * sin_angle + y * cos_angle;
-    x = new_x;
-
-    Point {
-        x: (x / z) * WINDOW_WIDTH as f64 / 2.0,
-        y: (y / z) * WINDOW_HEIGHT as f64 / 2.0,
-    }
-}
-
-fn calculate_lighting(x: f64, y: f64, z: f64, camera: &Camera) -> Color {
-    let light_direction = LIGHT_ANGLE.sin() * x + LIGHT_ANGLE.cos() * y;
-    let brightness = (light_direction + 1.0) / 2.0;
-    Color::RGB(
-        (brightness * 255.0) as u8,
-        (brightness * 255.0) as u8,
-        (brightness * 255.0) as u8,
-    )
-}
-
-struct Point {
-    x: f64,
-    y: f64,
+    Ok(())
 }
